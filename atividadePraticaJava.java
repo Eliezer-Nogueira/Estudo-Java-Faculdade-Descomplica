29f2bbf7cbc4a86301d624e3a32a8f1e1e0873fb
Eliezer Nogueira <85128107+Eliezer-Nogueira@users.noreply.github.com>
2023-05-23 14:49:56 -0300

    Update atividadePraticaJava.java
    Mudanças na forma de exibir os resultados

--- atividadePraticaJava.java
+++ atividadePraticaJava.java
@@ -2,21 +2,24 @@ import javax.swing.*;
 public class atividadePraticaJava {
 	
 	public static void main (String entrada[]) {
-		
+		//Declaraçãao de Variáveis
 		int number1, div, potencia;
 		double number2;
 		String textoDiv, textoPot;
 		
+		//Entrada de dados
 		number1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero:"));
 		number2 = Double.parseDouble(JOptionPane.showInputDialog("Digite outro numero:"));
 		
+		//Processamento
 		div = number1 / (int)number2;
 		potencia = (int)Math.pow(number1, number2);
 		
-		textoDiv = String.format("O resultado da divisão entre %d e %.2f é igual a %d", number1, number2, div);
-		textoPot = String.format("E o resultado da potenciação entre %d elevado a %.2f é igual a %d", number1, number2, potencia);
+		//Saída de Dados
+		textoDiv = String.format("O resultado da divisão entre %d e %.0f é igual a %d.", number1, number2, div);
+		textoPot = String.format(" E o resultado da potenciação entre %d elevado a %.0f é igual a %d", number1, number2, potencia);
 
-		System.out.println(div + " " + potencia);
+		JOptionPane.showMessageDialog(null, textoDiv + " " + textoPot);
 		
 		System.exit(0);
 	}
